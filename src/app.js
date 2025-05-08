aee7105bb6604c560fde84429b568d88978baec8
y10b <victory.jun01@gmail.com>
2025-05-08 13:22:11 +0900

    setting

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const router = require("./modules")
-const cors = require("cors")
-const app = express()
-const PORT = 5000
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = 5000;
 
 const corsOptions = {
     origin: 'http://localhost:3000',
     credentials: true,
 };
 
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(router)//index.js의 라우터 이용
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`서버가 작동 중입니다. ${PORT}`)
-})
+    console.log(`서버가 작동 중입니다. ${PORT}`);
+});
