82a97e43b1bfb77b5193f2762b4d77eb7217e005
y10b <victory.jun01@gmail.com>
2025-09-05 11:34:33 +0900

    fix: 게시글 image 필드 추가

--- src/db/prisma/migrations/20250905021852_init/migration.sql
+++ src/db/prisma/migrations/20250905021852_init/migration.sql
@@ -17,6 +17,7 @@ CREATE TABLE "Article" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
+    "images" TEXT,
     "authorId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
